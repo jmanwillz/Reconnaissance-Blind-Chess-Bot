9be9f9484d87e92afaff70a0500e4dc463b00fdd
Reecelaz <reecelaz.rl@outlook.com>
2024-04-20 20:32:14 +0200

    Part1_Sub1 Completed (Board representation)

--- Part_1/Part1_Sub1_Board_Representation.py
+++ Part_1/Part1_Sub1_Board_Representation.py
@@ -1,6 +1,12 @@
 #Part1 Sub1 Board Representation
-#Jason Wille (stuNo.#######), Kaylyn Karuppen (stuNo.#######), Reece Lazarus (2345362)
+#Jason Wille (1352200), Kaylyn Karuppen (2465081), Reece Lazarus (2345362)
+
+import chess
 
 state = []
 
 fen = input()
+
+board = chess.Board(fen)
+
+print(board)
