1eb3363b6a72d22504ee731cc47a0c7a358aec8e
Jason Wille <jason.wille@advance.io>
2024-05-07 00:09:58 +0200

    Make round robin not override files.

--- round_robin.sh
+++ round_robin.sh
@@ -16,14 +16,14 @@ num_runs=5
 
 # Loop through each set of arguments
 for arguments in "${argument_sets[@]}"; do
-    # Generate a timestamp
-    timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-    output_file="output_$timestamp.txt"
-    
     echo "Running with arguments: $arguments"
-    
+
     # Run the Python command multiple times with the same arguments
     for ((i=1; i<=$num_runs; i++)); do
+        # Generate a timestamp
+        timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+        output_file="output_$timestamp.txt"
+        
         echo "Run $i"
         $rc_bot_match_command $arguments >> "$output_file"
     done
